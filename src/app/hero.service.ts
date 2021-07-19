4a0059371167471939fb977aa2f3dc80934d2a31
pablomdd <pablo.mdd.007@gmail.com>
2021-07-18 22:42:00 -0500

    add update logic for hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -13,6 +13,9 @@ import { HEROES } from './mock-heroes';
 export class HeroService {
   private heroesUrl = 'api/heroes';  // URL to web api
 
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
   constructor(
     private http: HttpClient,
@@ -59,5 +62,13 @@ export class HeroService {
       // Let the app keep running by returning an empty result.
       return of(result as T);
     };
+  }
+
+  /** PUT: update the hero on the server */
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
   }
 }
