6fdd8c4bfd34260acd4723c4fd8d415ac67e4f14
pablomdd <pablo.mdd.007@gmail.com>
2021-07-18 21:41:56 -0500

    add heroDetail params from route, and new hero service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -8,10 +8,19 @@ import { HEROES } from './mock-heroes';
   providedIn: 'root'
 })
 export class HeroService {
+
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
     this.messageService.add('HeroService: fetched heroes');
     return heroes;
+  }
+
+  getHero(id: number): Observable<Hero> {
+    // For now, assume that a hero with the specified `id` always exists.
+    // Error handling will be added in the next step of the tutorial.
+    const hero = HEROES.find(h => h.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
   }
 
   constructor(private messageService: MessageService) { }
